4c02528b5c1932fccd99f5b1bb5712f76779e741
Petr Chalupa <chalupa.petr93@gmail.com>
2020-06-08 15:47:46 +0000

    Rename

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,17 +2,24 @@
 declare(strict_types = 1);
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManager;
 use App\Lib\Uri;
 use App\Controller\ArticleController;
+use App\View\Error;
+use Doctrine\ORM\EntityManager;
+use Exception;
 
-final class FrontController {
+
+final class AppController {
+	/** @var string */
 	private $version;
 	private $controller;
+	/** @var string */
 	private $action;
+	/** @var array */
 	private $params = array();
 
 	function __construct(EntityManager $entityManager){
+	try {
 		@list($uri, $params) = Uri::fromString($_SERVER["REQUEST_URI"]);
 		@list($version, $controller, $action) = $uri;
 		@list($action, $format) = explode(".", $action);
@@ -29,7 +36,6 @@ final class FrontController {
 			$this->setAction($action);
 		}
 
-
 		if(isset($params)) {
 			foreach(explode("&",$params) as $param) {
 				@list($name, $value) = explode("=", $param);
@@ -42,12 +48,16 @@ final class FrontController {
 
 		$action = $this->action;
 		$this->controller->$action($this->params);
+		} catch(Exception $e){
+			error_log($e->getMessage());
+			Error::render($e->getMessage(), 400);
+		}
 	}
 
-	private function setController($controller) {
+	private function setController(string $controller) {
         $controller = __NAMESPACE__ . "\\" . ucfirst(strtolower($controller)) . "Controller";
         if (!class_exists($controller)) {
-            throw new InvalidArgumentException(
+            throw new Exception(
                 "The controller '$controller' has not been defined.");
         }
 
@@ -55,10 +65,10 @@ final class FrontController {
     }
 
 
-	private function setAction($action) {
-        $action = strtolower($_SERVER['REQUEST_METHOD']) . ucfirst(strtolower($action));
+	private function setAction(string $action) {
+        $action = str_replace("_", "", ucwords(strtolower($action), "_"));
         if (!method_exists($this->controller, $action)) {
-            throw new InvalidArgumentException(
+            throw new Exception(
                 "The action '$action' has not been defined.");
         }
 
