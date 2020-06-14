136f68534ae8f11dad7c0a07be12c9079a47ae9c
Petr Chalupa <chalupa.petr93@gmail.com>
2020-06-14 06:49:00 +0000

    Uncomment content type header

--- src/View/BaseView.php
+++ src/View/BaseView.php
@@ -5,7 +5,7 @@ namespace App\View;
 class BaseView {
 	function __construct() {
 		header("Access-Control-Allow-Origin: *");
-		//header("Content-Type: application/json; charset=UTF-8");
+		header("Content-Type: application/json; charset=UTF-8");
 		header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 		header("Access-Control-Max-Age: 3600");
 		header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
