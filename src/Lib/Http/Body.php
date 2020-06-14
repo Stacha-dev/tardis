286b438e2110f4c252182118ddc1fa1f7016e3ec
Petr Chalupa <chalupa.petr93@gmail.com>
2020-06-14 12:50:50 +0000

    Implement decodeBody

--- src/Lib/Http/Body.php
+++ src/Lib/Http/Body.php
@@ -4,28 +4,37 @@ declare(strict_types=1);
 namespace App\Lib\Http;
 
 class Body {
-	/** @var string */
+	/** @var array<string> */
 	private $body = array();
 
 	public function __construct() {
-		$json = file_get_contents('php://input');
-		$this->setBody($json);
+		$body = file_get_contents('php://input');
+		if (is_string($body))
+			$this->body = $this->decodeBody($body) ?? array();
 	}
 
 	/**
 	 * Sets body by string.
 	 *
-	 * @param string $json
-	 * @return void
+	 * Type: 1-JSON
+	 * @param string $body
+	 * @param int $type
+	 * @return array<string>
 	 */
-	private function setBody(string $json) {
-		$this->body = json_decode($json, true);
+	private function decodeBody(string $body, int $type = 1): ?array {
+		switch($type) {
+			case 1:
+				$body = json_decode($body, true);
+			break;
+		}
+
+		return $body;
 	}
 
 	/**
 	 * Returns requests body.
 	 *
-	 * @return array
+	 * @return array<string>
 	 */
 	public function getBody(): array {
 		return $this->body;
@@ -35,7 +44,7 @@ class Body {
 	 * Returns body data by key.
 	 *
 	 * @param string $key
-	 * @return string
+	 * @return string|null
 	 */
 	public function getBodyData(string $key): ?string {
 		return array_key_exists($key, $this->body) ? $this->body[$key] : NULL;
