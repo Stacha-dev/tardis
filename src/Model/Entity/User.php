c576a8f59bdced9374b08b1d561441f95a5a4cce
Petr Chalupa <chalupa.petr93@gmail.com>
2020-06-18 14:54:45 +0000

    Update User entity

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -19,10 +19,46 @@ class User
     protected $id;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", length=191, unique=true)
+     * @var string
+     */
+    protected $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @ORM\Column(type="string", length=191, unique=true)
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
      * @var string
      */
     protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    protected $surname;
+
+    /**
+     * @ORM\Column(name="`date_created`", type="datetime")
+     * @var \DateTime
+     */
+    protected $dateCreated;
+
+    /**
+     * @ORM\COlumn(type="string", length=1024)
+     * @var string
+     */
+    protected $avatar;
 
 	/**
 	 * Return user ID.
@@ -32,6 +68,74 @@ class User
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Sets user username.
+     *
+     * @param string $username
+     * @return void
+     */
+    public function setUsername(string $username) {
+        $this->username = $username;
+    }
+
+    /**
+     * Returns user username.
+     *
+     * @return string
+     */
+    public function getUsername(): string {
+        return $this->username;
+    }
+
+    /**
+     * Sets user password.
+     *
+     * @param string $password
+     * @return void
+     */
+    public function setPassword(string $password) {
+        $this->password = $password;
+    }
+
+    /**
+     * Returns user password.
+     *
+     * @return string
+     */
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    /**
+     * Sets user e-mail.
+     *
+     * @param string $email
+     * @return void
+     */
+    public function setEmail(string $email) {
+        $this->email = $email;
+    }
+
+    /**
+     * Returns user e-mail.
+     *
+     * @return string
+     */
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    /**
+	 * Sets user name.
+	 *
+	 * @param string $name
+	 * @return void
+	 */
+    public function setName(string $name)
+    {
+        $this->name = $name;
     }
 
 	/**
@@ -44,14 +148,62 @@ class User
         return $this->name;
     }
 
-	/**
-	 * Sets user name.
-	 *
-	 * @param string $name
-	 * @return void
-	 */
-    public function setName(string $name)
-    {
-        $this->name = $name;
+    /**
+     * Sets user surname.
+     *
+     * @param string $surname
+     * @return void
+     */
+    public function setSurname(string $surname) {
+        $this->surname = $surname;
     }
+
+    /**
+     * Returns user surname.
+     *
+     * @return string
+     */
+    public function getSurname():string {
+        return $this->surname;
+    }
+
+    /**
+     * Sets user created date.
+     *
+     * @param \DateTime $dateCreated
+     * @return void
+     */
+    public function setDateCreated(\DateTime $dateCreated) {
+        $this->dateCreated = $dateCreated;
+    }
+
+    /**
+     * Returns user created date.
+     *
+     * @return \DateTime
+     */
+    public function getDateCreated(): \DateTime {
+        return $this->dateCreated;
+    }
+
+    /**
+     * Sets user avatar.
+     *
+     * @param string $avatar
+     * @return void
+     */
+    public function setAvatar(string $avatar) {
+        $this->avatar = $avatar;
+    }
+
+    /**
+     * Returns user avatar.
+     *
+     * @return string
+     */
+    public function getAvatar(): string {
+        return $this->avatar;
+    }
+
+
 }
