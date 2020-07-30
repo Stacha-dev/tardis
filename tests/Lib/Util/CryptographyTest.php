447dd2b6d24a20e08e4d83476b7ae8aec10aa5b4
Petr Chalupa <chalupa.petr93@gmail.com>
2020-07-30 05:04:46 +0000

    Add cryptography test

---
+++ tests/Lib/Util/CryptographyTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+use App\Lib\Util\Cryptography;
+
+class CryptographyTest extends TestCase
+{
+    /**
+     * Test is successfull if random string is generated.
+     *
+     * @return void
+     */
+    public function testRandomCanBeGenerated()
+    {
+        $randoms = array();
+        foreach (range(1, 10) as $i) {
+            $random = Cryptography::random(10);
+            $this->assertIsString($random);
+            $this->assertNotContains($random, $randoms);
+            array_push($randoms, $random);
+        }
+    }
+
+    /**
+     * Test is successfull if string is hashed.
+     *
+     * @return void
+     */
+    public function testStringCanBeHashed()
+    {
+        $this->assertIsString(Cryptography::hashHmac("foo", "bar"));
+    }
+}
