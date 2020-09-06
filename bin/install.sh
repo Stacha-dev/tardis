f26f8f2377dc614a25dc6baee4e20c5938ce89cc
Petr Chalupa <chalupa.petr93@gmail.com>
2020-09-06 16:33:43 +0000

    Update install to generate DB config file

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,27 @@
-#!/bin/sh
-echo Install...
+#!/usr/bin/env bash
+
+if [ "$#" -ne 4 ]; then
+	exit 1;
+fi
+
+DRIVER=$1
+USER=$2
+PASSWORD=$3
+DBNAME=$4
 
 # Composer
 cd ..
 composer install
 
-# ORM
+# DB
+cd config
+function generate_config() {
+  eval "echo \"$(cat $1)\""
+}
+
+generate_config common.ini.tmpl > common.ini
+
+cd ..
 composer orm:drop
 composer orm:create
 
@@ -14,4 +30,5 @@ cd resources/admin
 npm ci
 npm run build:prod
 
+exit 0;
 echo Done!
