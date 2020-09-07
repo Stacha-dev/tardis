284ee528d4f86581f48b81a7fa63c0e2f2e144dc
Petr Chalupa <chalupa.petr93@gmail.com>
2020-09-07 09:27:06 +0200

    Uprade (#22)
    * Update
    
    * Update dependecies
    
    * Register default routes (#16)
    
    * Add default routes
    
    * Test register default routes method
    
    * Change build command
    
    * Update
    
    * Add create db config  (#17)
    
    * Update install to generate DB config file
    
    * Remove echo
    
    * Add composer validate
    
    * Add install step
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Uodate
    
    * Update
    
    * Update
    
    * Update
    
    * Develop db install script (#18)
    
    * Update install to generate DB config file
    
    * Remove echo
    
    * Add composer validate
    
    * Add install step
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Uodate
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Develop db install script (#19)
    
    * Update install to generate DB config file
    
    * Remove echo
    
    * Add composer validate
    
    * Add install step
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Uodate
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Add DB credetials from secrets
    
    * Update
    
    * Remove one driver param
    
    * Change host
    
    * Update
    
    * Add port
    
    * Change image
    
    * Update
    
    * Update
    
    * Add port
    
    * Add host ip address
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Add php unit test to ci (#20)

--- bin/install.sh
+++ bin/install.sh
@@ -1,17 +1,35 @@
-#!/bin/sh
-echo Install...
+#!/usr/bin/env bash
+
+if [ "$#" -ne 5 ]; then
+	exit 1;
+fi
+
+USER=$1
+PASSWORD=$2
+DBNAME=$3
+HOST=$4
+PORT=$5
 
 # Composer
 cd ..
+composer validate
 composer install
 
-# ORM
+# DB
+cd config
+function generate_config() {
+  eval "echo \"$(cat $1)\""
+}
+
+generate_config common.ini.tmpl > common.ini
+
+cd ..
 composer orm:drop
 composer orm:create
 
 # NPM
 cd resources/admin
 npm ci
-npm run build:production
+npm run build:prod
 
-echo Done!
+exit 0;
