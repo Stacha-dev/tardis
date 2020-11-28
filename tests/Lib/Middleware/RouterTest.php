a5f022141d6c9c88bc4beabfe0c904223a3ab37a
Petr Chalupa <chalupa.petr93@gmail.com>
2020-11-28 07:19:54 +0000

    Remove setup method

--- tests/Lib/Middleware/RouterTest.php
+++ tests/Lib/Middleware/RouterTest.php
@@ -6,13 +6,6 @@ use App\Lib\Middleware\RouteFactory;
 
 final class RouterTest extends TestCase
 {
-    private $router;
-
-    public function setUp(): void
-    {
-        $this->router = new  \App\Lib\Middleware\Router;
-    }
-
     /**
      * Test is successfull if route is registered and instance of Router is returned.
      *
@@ -20,7 +13,8 @@ final class RouterTest extends TestCase
      */
     public function testRouteCanBeRegistered(): void
     {
-        $this->assertInstanceOf("App\Lib\Middleware\Router", $this->router->register(RouteFactory::fromConstants(1, "GET", "@^(?<version>[0-9])/article$@", "getAll", array(), false)));
-        $this->assertCount(1, $this->router->getRoutes());
+        $router = new  \App\Lib\Middleware\Router;
+        $this->assertInstanceOf("App\Lib\Middleware\Router", $router->register(RouteFactory::fromConstants(1, "GET", "@^(?<version>[0-9])/article$@", "getAll", array(), false)));
+        $this->assertCount(1, $router->getRoutes());
     }
 }
