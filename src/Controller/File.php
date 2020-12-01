b00115802617d834918d9a0350d427a52e3eb61b
Petr Chalupa <chalupa.petr93@gmail.com>
2020-12-01 20:25:57 +0000

    Add get all and get one methods

--- src/Controller/File.php
+++ src/Controller/File.php
@@ -17,7 +17,38 @@ final class File extends \App\Controller\Base
      */
     public function registerRoutes(\App\Lib\Middleware\Router $router): void
     {
-        $router->register(RouteFactory::fromConstants(1, "POST", "@^(?<version>[0-9]+)/file$@", "upload", [], true));
+        $router->register(RouteFactory::fromConstants(1, "GET", "@^(?<version>[0-9])/file$@", "getAll"))
+                ->register(RouteFactory::fromConstants(1, "POST", "@^(?<version>[0-9]+)/file$@", "upload", [], true));
+    }
+
+    /**
+     * Gets all files
+     *
+     * @return array<array>
+     */
+    public function getAll(): array
+    {
+        $query = $this->entityManager->createQuery('SELECT a FROM App\Model\Entity\File a');
+        $result = $query->getArrayResult();
+        $this->view->render($result);
+        return $result;
+    }
+
+    /**
+     * Gets one file by ID
+     *
+     * @param  int $id
+     * @return \App\Model\Entity\File
+     */
+    public function getOneById(int $id=0): \App\Model\Entity\File
+    {
+        $result = $this->entityManager->find('App\Model\Entity\File', $id);
+        if ($result instanceof \App\Model\Entity\File) {
+            $this->view->render(array('id' => $result->getId(), 'title' => $result->getTitle(), 'path' => $result->getPath(), 'state' => $result->getState()));
+            return $result;
+        } else {
+            throw new Exception("File by ID can not be founded!");
+        }
     }
 
     /**
@@ -25,11 +56,15 @@ final class File extends \App\Controller\Base
      *
      * @return void
      */
-    public function upload(): void
+    public function upload(string $title = ""): void
     {
-        foreach ($this->request->getBody()->getFiles() as $file) {
+        $body = $this->request->getBody();
+        $title = $body->getBodyData('title') ?? $title;
+        foreach ($body->getFiles() as $file) {
             FileSystem::upload($file);
             $this->view->render(["path"=>FileSystem::getUri($file)]);
+            $fileEntity = new \App\Model\Entity\File($title, FileSystem::getUri($file));
         }
+        $this->entityManager->flush();
     }
 }
