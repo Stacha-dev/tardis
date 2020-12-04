ae532c23af7c2b2e5a5b6d60708997b95d968516
oliverstasa <oliver.stasa@gmail.com>
2020-12-04 17:47:41 +0100

    Update #38

--- src/Lib/Exeption/Exeption.php
+++ src/Lib/Exeption/Exeption.php
@@ -2,41 +2,42 @@
 declare(strict_types = 1);
 namespace App;
 
-class EinExeption extends Exeption {
+class EinExeption
+{
 
-  /** @var string */
-  private $message = 'Unknown exeption';
+    /** @var string */
+    private $message = 'Unknown exeption';
 
-  /** @var int */
-  private $code = 0;
+    /** @var int */
+    private $code = 0;
 
-  /**
-  * @param string $message
-  * @param int $code
-  */
-  public function __construct(string $message, int $code) {
+    /**
+    * @param string $message
+    * @param int $code
+    */
+    public function __construct(string $message, int $code)
+    {
+        $this->message = $message;
+        $this->code = $code;
+    }
 
-    $this->message = $message;
-    $this->code = $code;
+    /**
+    * Returns message from exeption
+    *
+    * @return string
+    */
+    public function getMessage():string
+    {
+        return $this->message;
+    }
 
-  }
-
-  /**
-  * Returns message from exeption
-  *
-  * @return string
-  */
-  public function getMessage():string {
-    return $this->message;
-  }
-
-  /**
-  * Returns code of exeption from exeption
-  *
-  * @return int
-  */
-  public function getCode():int {
-    return $this->code;
-  }
-
+    /**
+    * Returns code of exeption from exeption
+    *
+    * @return int
+    */
+    public function getCode():int
+    {
+        return $this->code;
+    }
 }
