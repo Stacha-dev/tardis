cc63b14152cf9244aac52f9c4ff007a5b28e7fb4
oliverstasa <oliver.stasa@gmail.com>
2020-12-12 12:32:04 +0000

    whirpooool hash, non-active salt

---
+++ src/Lib/Security/Hash.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types = 1);
+namespace App\Lib\Security; // pak vyuziti bude use App\Lib\Security\Hash;
+
+// use App\Lib\Configuration\ConfigurationFactory;
+//$configuration = ConfigurationFactory::fromFileName('common');
+//$salt = $configuration->get('jwt_key'); // change to salt_key?
+
+class Hash
+{
+
+
+  /**
+  * Creates hash from string.
+  *
+  * @return string
+  */
+    public static function getHash(string $str):string
+    {
+
+        $salt = 'hellomysalt';
+
+        $res = hash('whirlpool', $salt.$str);
+
+        return $res;
+    }
+}
