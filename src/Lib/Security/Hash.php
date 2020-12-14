dacbb9f332691392411a1aa1d23857925e21714e
oliverstasa <oliver.stasa@gmail.com>
2020-12-14 18:46:00 +0000

    Fix comments

--- src/Lib/Security/Hash.php
+++ src/Lib/Security/Hash.php
@@ -22,55 +22,47 @@ class Hash
     }
 
 
-  /**
-  * Basic hash for pepper and password before submit/verify.
-  *
-  * @return string
-  */
-  private static function hashString(string $str):string
-  {
-      return hash('md5', $str);
-  }
-
-
-  /**
-  * Creates hash from string.
-  *
-  * @return string
-  */
-  public static function getHash(string $str):string
-  {
-
-    // check for maximum password length
-    if (strlen($str) > 32) {
-        throw new Exception('Submitted password is too long.', 401);
-    } else {
-
-    // hash user input
-    $str = self::hashString($str);
-    $pepper = self::hashString(self::getPepper());
-
-    // hash pepper with input together
-    $res = password_hash($pepper.$str, PASSWORD_ARGON2I);
-
-    return $res?$res:'';
+    /**
+    * Basic hash for pepper and password before submit/verify.
+    *
+    * @return string
+    */
+    private static function hashString(string $str):string
+    {
+        return hash('md5', $str);
     }
-  }
 
 
-  /**
-  * Verify hash from string.
-  *
-  * @return bool
-  */
-  public static function verifyHash(string $str, string $hash):bool
-  {
+    /**
+    * Creates hash from string.
+    *
+    * @return string
+    */
+    public static function getHash(string $str):string
+    {
+        if (strlen($str) > 32) {
+            throw new Exception('Submitted password is too long.', 401);
+        }
 
-    // hash user input
-    $str = self::hashString($str);
-    $pepper = self::hashString(self::getPepper());
+        $str = self::hashString($str);
+        $pepper = self::hashString(self::getPepper());
 
-    // hash pepper with input together
-    return password_verify($pepper.$str, $hash);
-  }
+        $res = password_hash($pepper.$str, PASSWORD_ARGON2I);
+
+        return $res?$res:'';
+    }
+
+
+    /**
+    * Verify hash from string.
+    *
+    * @return bool
+    */
+    public static function verifyHash(string $str, string $hash):bool
+    {
+        $str = self::hashString($str);
+        $pepper = self::hashString(self::getPepper());
+
+        return password_verify($pepper.$str, $hash);
+    }
 }
