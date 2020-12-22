2b886a5302fe348c4e8f2274d5059956918da822
Petr Chalupa <chalupa.petr93@gmail.com>
2020-12-22 11:29:52 +0000

    Add confirmation question

--- src/Command/FixturesLoadCommand.php
+++ src/Command/FixturesLoadCommand.php
@@ -4,6 +4,8 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\Common\DataFixtures\Loader;
@@ -21,6 +23,7 @@ class FixturesLoadCommand extends Command
     protected function configure():void
     {
         $this->setDescription('Loads all fixtures.')->setHelp('This command allows you to load all fixtures.');
+        $this->addOption('yes', 'y', InputOption::VALUE_NONE, 'Continue with yes?');
     }
 
     /**
@@ -32,11 +35,19 @@ class FixturesLoadCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $loader = new Loader();
-        $loader->loadFromDirectory(__DIR__ . '/../Model/Fixtures');
-        $purger = new ORMPurger();
-        $executor = new ORMExecutor(\App\Bootstrap::getEntityManager(), $purger);
-        $executor->execute($loader->getFixtures());
-        return Command::SUCCESS;
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion('This action deletes all contents of the database. Do you want to continue? [yes/no]' . PHP_EOL, false);
+
+        if ($input->getOption('yes') || $helper->ask($input, $output, $question)) {
+            $loader = new Loader();
+            $loader->loadFromDirectory(__DIR__ . '/../Model/Fixtures');
+            $purger = new ORMPurger();
+            $executor = new ORMExecutor(\App\Bootstrap::getEntityManager(), $purger);
+            $executor->execute($loader->getFixtures());
+
+            return Command::SUCCESS;
+        }
+
+        return Command::FAILURE;
     }
 }
