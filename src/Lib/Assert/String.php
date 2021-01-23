243390417533be21188f95ce51c867a313f9ecf9
oliverstasa <oliver.stasa@gmail.com>
2021-01-23 13:11:11 +0000

    Update class fix

--- src/Lib/Assert/String.php
+++ src/Lib/Assert/String.php
@@ -1,14 +1,18 @@
 <?php
-declare(strict_types = 1);
+
+declare(strict_types=1);
+
 namespace App\Lib\Assert;
 
-
-/**
- * Test string for email address
- * 
- * @return bool
- */
-public function isEmail(string $email):bool
+class Text
 {
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
+    /**
+     * Test string for email address
+     *
+     * @return bool
+     */
+    public static function isEmail(string $email): bool
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL) === false ? false : true;
+    }
 }
