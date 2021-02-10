12b4d864c5f5ceb075041f0321668c3a28a15994
Petr Chalupa <chalupa.petr93@gmail.com>
2021-02-10 20:39:04 +0000

    Add tests method

--- tests/Lib/FileSystem/FileTest.php
+++ tests/Lib/FileSystem/FileTest.php
@@ -1,5 +1,7 @@
 <?php
+
 declare(strict_types=1);
+
 use PHPUnit\Framework\TestCase;
 use App\Lib\FileSystem\File;
 
@@ -8,19 +10,20 @@ final class FileTest extends TestCase
     /** @var string */
     private const DOCUMENT_ROOT = __DIR__ . "/../../..";
 
-    private const FILENAME = "test_file";
+    /** @var string */
+    private const SAMPLE_FILENAME = "test_file";
 
     /** @var string */
-    private const FILE_PATH = self::DOCUMENT_ROOT . "/public/storage/" . self::FILENAME . ".pdf";
+    private const SAMPLE_FILE_PATH = self::DOCUMENT_ROOT . "/public/storage/" . self::SAMPLE_FILENAME . ".pdf";
 
     public function setUp(): void
     {
-        fopen(self::FILE_PATH, 'w');
+        fopen(self::SAMPLE_FILE_PATH, 'w');
     }
 
-    public function tearDown():void
+    public function tearDown(): void
     {
-        @unlink(self::FILE_PATH);
+        @unlink(self::SAMPLE_FILE_PATH);
     }
 
     /**
@@ -28,9 +31,9 @@ final class FileTest extends TestCase
      *
      * @return void
      */
-    public function testFileCanBeOpened():void
+    public function testFileCanBeOpened()
     {
-        $file = new File(self::FILE_PATH);
+        $file = new File(self::SAMPLE_FILE_PATH);
         $this->assertInstanceOf("App\Lib\FileSystem\File", $file);
     }
 
@@ -39,9 +42,9 @@ final class FileTest extends TestCase
      *
      * @return void
      */
-    public function testFileExists():void
+    public function testFileExists()
     {
-        $file = new File(self::FILE_PATH);
+        $file = new File(self::SAMPLE_FILE_PATH);
         $this->assertFileExists($file->getPath());
     }
 
@@ -50,10 +53,10 @@ final class FileTest extends TestCase
      *
      * @return void
      */
-    public function testFileNameIsReturned():void
+    public function testFileNameIsReturned()
     {
-        $file = new File(self::FILE_PATH);
-        $this->assertSame(self::FILENAME, $file->getFilename());
+        $file = new File(self::SAMPLE_FILE_PATH);
+        $this->assertSame(self::SAMPLE_FILENAME, $file->getFilename());
     }
 
     /**
@@ -61,9 +64,9 @@ final class FileTest extends TestCase
      *
      * @return void
      */
-    public function testFileIsImage():void
+    public function testFileIsImage()
     {
-        $file = new File(self::FILE_PATH);
+        $file = new File(self::SAMPLE_FILE_PATH);
         $this->assertFalse($file->isImage());
     }
 
@@ -72,9 +75,9 @@ final class FileTest extends TestCase
      *
      * @return void
      */
-    public function testFileToImage():void
+    public function testFileToImage()
     {
-        $file = new File(self::FILE_PATH);
+        $file = new File(self::SAMPLE_FILE_PATH);
         $this->expectException("Exception");
         $image = $file->toImage();
     }
@@ -84,9 +87,9 @@ final class FileTest extends TestCase
      *
      * @return void
      */
-    public function testFileCanBeDeleted():void
+    public function testFileCanBeDeleted()
     {
-        $file = new File(self::FILE_PATH);
+        $file = new File(self::SAMPLE_FILE_PATH);
         $file->delete();
         $this->assertFileNotExists($file->getPath());
     }
@@ -96,9 +99,9 @@ final class FileTest extends TestCase
      *
      * @return void
      */
-    public function testFileCanBeRenamed():void
+    public function testFileCanBeRenamed()
     {
-        $file = new File(self::FILE_PATH);
+        $file = new File(self::SAMPLE_FILE_PATH);
         $file->rename("new_name");
         $this->assertFileExists($file->getPath());
         $file->delete();
@@ -109,11 +112,85 @@ final class FileTest extends TestCase
      *
      * @return void
      */
-    public function testFileCanBeMoved():void
+    public function testFileCanBeMoved()
     {
-        $file = new File(self::FILE_PATH);
+        $file = new File(self::SAMPLE_FILE_PATH);
         $file->move(self::DOCUMENT_ROOT . "/public/storage/test.pdf");
         $this->assertFileExists($file->getPath());
         $file->delete();
+    }
+
+    /**
+     * Test is successful if file can be copied
+     *
+     * @return void
+     */
+    public function testFileCanBeCopied()
+    {
+        $file = new File(self::SAMPLE_FILE_PATH);
+        $fileCopy = $file->copy(self::DOCUMENT_ROOT . "/public/storage/copy.pdf");
+        $this->assertInstanceOf("\App\Lib\FileSystem\File", $fileCopy);
+        $this->assertFileExists($fileCopy->getPath());
+        $fileCopy->delete();
+    }
+
+    /**
+     * TEst is successful if file can be cloned
+     *
+     * @return void
+     */
+    public function testFileCanBeCloned()
+    {
+        $file = new File(self::SAMPLE_FILE_PATH);
+        $fileClone = $file->clone();
+        $this->assertInstanceOf("\App\Lib\FileSystem\File", $fileClone);
+        $this->assertFileExists($file->getPath());
+        $fileClone->delete();
+    }
+
+    /**
+     * Test is successful if file basename can be returned
+     *
+     * @return void
+     */
+    public function testFileBasenameCanBeReturned()
+    {
+        $file = new File(self::SAMPLE_FILE_PATH);
+        $this->assertEquals(self::SAMPLE_FILENAME . ".pdf", $file->getBasename());
+    }
+
+    /**
+     * Test is successful if mime type can be returned
+     *
+     * @return void
+     */
+    public function testFileMimeTypeCanBeReturned()
+    {
+        $file = new File(self::SAMPLE_FILE_PATH);
+        $this->assertEquals("application/x-empty", $file->getMimeType());
+    }
+
+    /**
+     * Test is successful if upload name can be returned
+     *
+     * @return void
+     */
+    public function testUploadNameCanBeSetted()
+    {
+        $file = new File(self::SAMPLE_FILE_PATH);
+        $file->setUploadName("upload_name");
+        $this->assertEquals("upload_name", $file->getUploadName());
+    }
+
+    /**
+     * Test is successful if file to image can not be converted
+     *
+     * @return void
+     */
+    public function testFileToImageCanBeConverted()
+    {
+        $file = new File(self::SAMPLE_FILE_PATH);
+        $this->expectException("Exception");
+        $image = $file->toImage();
     }
 }
